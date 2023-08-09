7616d6dc9ce65296955fb82bd348e880b4777ebb
ymkim97 <rladudaud97@naver.com>
2023-08-09 16:07:08 +0900

    Entity class 추가

---
+++ jpabook/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Address {
+
+	private String city;
+	private String street;
+	private String zipcode;
+}
---
+++ jpabook/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Table(name = "item")
+public abstract class Item {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "item_id")
+	private Long id;
+
+	private String name;
+	private int price;
+	private int stockQuantity;
+
+	@ManyToMany(mappedBy = "items")
+	private List<Category> categories = new ArrayList<>();
+}
